87f91343ca303eb0bb96a017de4cd78c9e442b21
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-03-02 23:58:04 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_mini_crypto8_unified (
+module tt_um_crypto8_unified (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
