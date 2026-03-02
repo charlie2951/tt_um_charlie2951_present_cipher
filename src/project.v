4ea17dda8b2abf822ffcc94f3b66fd2156ddebfb
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-03-02 23:56:44 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_mini_crypto8_unified (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,20 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
+//  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+//  assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+  wire _unused = &{ena, 1'b0};
+//Instantiate crypto module
+    crypto8_unified cipher0(
+        .clk(clk),
+        .rst(!rst_n),
+        .data_in(ui_in),
+        .key(uio_in[5:0]),//6 bit key
+        .start(uio_in[6]),//start signal
+        .enc_dec(uio_in[7]),   // 1=encrypt, 0=decrypt
+        .data_out(uo_out) //result
+);
 endmodule
